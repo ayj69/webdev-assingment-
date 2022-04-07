c17b1a6fb698647718576a156dbfd0010892fdc3
ayj69 <86646606+ayj69@users.noreply.github.com>
2022-04-08 01:52:18 +0800

    Delete assignment2 directory

--- assignment2/src/logout.php
+++
@@ -1,6 +0,0 @@
-<?php  
-
-session_start();//session is a way to store information (in variables) to be used across multiple pages.  
-session_destroy();  
-redirect_to('/assignment2/index.php');//use for the redirection to some page  
-?>  
