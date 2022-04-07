3b08da0e531911a56ae9173b4edecb574b4ba2a8
ayj69 <86646606+ayj69@users.noreply.github.com>
2022-04-08 01:24:48 +0800

    merge with avas

--- assignment2/admin/useradmin/index.php
+++ assignment2/admin/useradmin/index.php
@@ -46,7 +46,7 @@ require __DIR__ . '/../../src/bootstrap.php';
 		$statement = db()->query("SELECT * FROM users");
 		$results =  $statement->fetchall(PDO::FETCH_ASSOC);	
 	?>
-    <div class="admin-table">
+    
     <h1 class="page-title">Manage User</h1>
     <table>
         <thead>
@@ -62,39 +62,38 @@ require __DIR__ . '/../../src/bootstrap.php';
                 <td><?php echo $row['username']; ?></td>
                 <td><?php echo $row['email']; ?></td>
                 <td>
-                    <a href="index.php?edit=<?php echo $row['id']; ?>" class="edit_btn" >Edit</a>
+                    <a href="useradmin.php?edit=<?php echo $row['id']; ?>" class="edit_btn" >Edit</a>
                 </td>
                 <td>
-                    <a href="../../src/userserver.php?del=<?php echo $row['id']; ?>" class="del_btn">Delete</a>
+                    <a href="userserver.php?del=<?php echo $row['id']; ?>" class="del_btn">Delete</a>
                 </td>
             </tr>
         <?php } ?>
     </table>
-    </div>
-    <div class="input-group">
-    <h1 class="page-title">Create User</h1>
-	<form method="post" action="../../src/userserver.php" >
+
+	<form method="post" action="userserver.php" >
         <input type="hidden" name="id" value="<?php echo $id; ?>">
 
-             <div class="input-field">
-                 <label>Username</label>
+             <div class="input-group">
+                 <label>username</label>
                  <input type="text" name="username" value="<?php echo $username; ?>">
 
              </div>
-             <div class="input-field">
-                <label>Email</label>
+             <div class="input-group">
+                <label>email</label>
                 <input type="text" name="email" value="<?php echo $email; ?>">
 
             </div>
-             <div class="input-field">
+             <div class="input-group">
              <?php if ($update == true): ?>
-            	<button class="btn" type="submit" name="update" style="background: #556B2F;" >Update</button>
+            	<button class="btn" type="submit" name="update" style="background: #556B2F;" >update</button>
             <?php else: ?>
             	<button class="btn" type="submit" name="save" >Save</button>
              <?php endif ?>          
                </div>
          </form>
 
+         <hr>
 
 
 
